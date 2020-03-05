60a9b14e8e1008b1bf26a9f420b6c73a3210dc35
Julien Palmas <julien@crx.io>
2020-03-05 18:08:18 +0100

    dynamic resolution of fuiLess

--- packages/ember-cli-fui-less/index.js
+++ packages/ember-cli-fui-less/index.js
@@ -1,4 +1,5 @@
 'use strict';
+const resolve = require('resolve');
 
 module.exports = {
   name: require('./package').name,
@@ -9,8 +10,12 @@ module.exports = {
     if (!app.options["lessOptions"]) app.options["lessOptions"] = {};
     if (!app.options["lessOptions"]["paths"]) app.options["lessOptions"]["paths"] = [];
 
+    const accordionModulePath = "/definitions/modules/accordion.js";
+    let res = resolve.sync(`fomantic-ui-less${accordionModulePath}`, { basedir: __dirname });
+    const fuiLessPath = res.replace(accordionModulePath, '');
+
     app.options["lessOptions"]["paths"].push(
-      'node_modules/fomantic-ui-less',
+      fuiLessPath,
       './app/styles/fomantic',
       './app/styles/fomantic/dummy-path-to-import/theme.config'
     );
