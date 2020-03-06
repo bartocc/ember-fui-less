76320e30682bc2ebb5ccb84a2f97568758b59c94
Julien Palmas <julien@crx.io>
2020-03-06 10:35:11 +0100

    catch addon and package addition errors

--- packages/ember-fui-less/blueprints/ember-fui-less/index.js
+++ packages/ember-fui-less/blueprints/ember-fui-less/index.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
-const resolve = require('resolve');
+const fs = require("fs");
+const resolve = require("resolve");
+const debug = require("debug")("ember-fui-less:default-blueprint");
 
 // fs.existsSync(`${__dirname}/js/addon.js`);
 
@@ -14,35 +15,84 @@ module.exports = {
   },
 
   async beforeInstall(options, locals) {
-    await this.addAddonToProject({ name: 'ember-cli-less' });
-    await this.addPackageToProject('fomantic-ui-less');
+    try {
+      await this.addAddonToProject({ name: "ember-cli-less" });
+    } catch (error) {
+      debug("Could not add `ember-cli-less` addon to project: %o", error);
+    }
+
+    try {
+      await this.addPackageToProject("fomantic-ui-less");
+    } catch (error) {
+      debug("Could not add `fomantic-ui-less` package to project: %o", error);
+    }
 
     const fomanticDestDir = `__root__/styles/fomantic`;
 
     // Resolve the path of fomantic-ui-less/theme.config.example and copy the file
     // to the blueprints files dir with name theme.config
-    let res = resolve.sync('fomantic-ui-less/theme.config.example', { basedir: __dirname });
+    let res = resolve.sync("fomantic-ui-less/theme.config.example", {
+      basedir: __dirname
+    });
     fs.copyFileSync(res, `${this.filesPath()}/${fomanticDestDir}/theme.config`);
 
     // res = resolve.sync('fomantic-ui-less/_site/globals/site.variables', { basedir: __dirname });
     // fs.copyFileSync(res, `${this.filesPath()}/${fomanticDestDir}/site/globals/site.variables`);
 
-    res = resolve.sync('fomantic-ui-less/semantic.less', { basedir: __dirname });
-    fs.copyFileSync(res, `${this.filesPath()}/${fomanticDestDir}/semantic.less`);
+    res = resolve.sync("fomantic-ui-less/semantic.less", {
+      basedir: __dirname
+    });
+    fs.copyFileSync(
+      res,
+      `${this.filesPath()}/${fomanticDestDir}/semantic.less`
+    );
 
-    await this.insertIntoFile(`${locals.fileMap["__root__"]}/styles/app.less`, "@import 'app/styles/fomantic/semantic';");
-    await this.insertIntoFile(`${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`, "/* Uncomment below the FUI elements you need", {after: "/* Elements */"});
-    await this.insertIntoFile(`${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`, "end of elements whitelist */", {before: "/* Collections */"});
-    await this.insertIntoFile(`${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`, "/* Uncomment below the FUI collections you need", {after: "/* Collections */"});
-    await this.insertIntoFile(`${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`, "end of collections whitelist */", {before: "/* Views */"});
-    await this.insertIntoFile(`${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`, "/* Uncomment below the FUI Views you need", {after: "/* Views */"});
-    await this.insertIntoFile(`${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`, "end of views whitelist */", {before: "/* Modules */"});
-    await this.insertIntoFile(`${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`, "/* Uncomment below the FUI Modules you need", {after: "/* Modules */"});
-    await this.insertIntoFile(`${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`, "end of modules whitelist */");
+    await this.insertIntoFile(
+      `${locals.fileMap["__root__"]}/styles/app.less`,
+      "@import 'app/styles/fomantic/semantic';"
+    );
+    await this.insertIntoFile(
+      `${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`,
+      "/* Uncomment below the FUI elements you need",
+      { after: "/* Elements */" }
+    );
+    await this.insertIntoFile(
+      `${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`,
+      "end of elements whitelist */",
+      { before: "/* Collections */" }
+    );
+    await this.insertIntoFile(
+      `${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`,
+      "/* Uncomment below the FUI collections you need",
+      { after: "/* Collections */" }
+    );
+    await this.insertIntoFile(
+      `${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`,
+      "end of collections whitelist */",
+      { before: "/* Views */" }
+    );
+    await this.insertIntoFile(
+      `${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`,
+      "/* Uncomment below the FUI Views you need",
+      { after: "/* Views */" }
+    );
+    await this.insertIntoFile(
+      `${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`,
+      "end of views whitelist */",
+      { before: "/* Modules */" }
+    );
+    await this.insertIntoFile(
+      `${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`,
+      "/* Uncomment below the FUI Modules you need",
+      { after: "/* Modules */" }
+    );
+    await this.insertIntoFile(
+      `${locals.fileMap["__root__"]}/styles/fomantic/semantic.less`,
+      "end of modules whitelist */"
+    );
   },
 
   async afterInstall() {
     this.files().forEach(path => fs.unlinkSync(`${this.filesPath()}/${path}`));
   }
-
 };
